dbef812c8fc34b4b93cfdf9fc0934989c1b8b448
Haborym <krafteck666@gmail.com>
2022-08-09 12:28:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ window.onload = function() {
     let canvas = document.getElementById('testCouleur');
     let context = canvas.getContext('2d');
 
+    let height = -1;
+    let width = -1;
+
     let arr_data = null;
 
     make_base();
@@ -14,20 +17,31 @@ window.onload = function() {
     function make_base() { 
         base_image = new Image();
         base_image.crossOrigin = "anonymous";
-        base_image.src = 'https://socialboulder.s3-eu-west-1.amazonaws.com/bouldersPics/vMb6jjcfe7arsqoLq.jpg';
-        // base_image.src = 'route.jpg';
+        // base_image.src = 'https://socialboulder.s3-eu-west-1.amazonaws.com/bouldersPics/vMb6jjcfe7arsqoLq.jpg';
+        // base_image.src = 'https://socialboulder.s3-eu-west-1.amazonaws.com/bouldersPics/FBK4zna8FL4fmki4t.jpg';
+        base_image.src = 'https://socialboulder.s3-eu-west-1.amazonaws.com/bouldersPics/96cNmdZ8c8KCuSA8n.jpg';
         base_image.onload = function(){
+            height = base_image.height;
+            width = base_image.width;
             context.drawImage(
                 base_image, 0, 0, base_image.width, base_image.height,     // source rectangle
-                0, 0, canvas.width, canvas.height
+                0, 0, canvas.width - 200, canvas.height
+                // 0, 0, canvas.width - 200, canvas.height
             );
 
             arr_data = context.getImageData(0, 0, canvas.width, canvas.height);
         }
     }
 
+    canvas.addEventListener('mousemove', event => {
+        const pixel = context.getImageData(event.x, event.y, 1, 1);
+        const data_p = pixel.data;
+        context.fillStyle = `rgb(${data_p[0]}, ${data_p[1]}, ${data_p[2]})`
+        context.fillRect(canvas.width - 200, 0, 150, 150);
+    });
+
     canvas.addEventListener('click', event => {
-        // console.log(event);
+        console.log(`x:${event.x}, y:${event.y}`);
         
         const pixel = context.getImageData(event.x, event.y, 1, 1);
         const data_p = pixel.data;
@@ -39,17 +53,48 @@ window.onload = function() {
         if(first) {
             first = false;
             xBase = temp_x;
-        } else {
-            console.log(deltaE(xBase, temp_x));
-        }        
+            // context.fillStyle = `rgb(${data_p[0]}, ${data_p[1]}, ${data_p[2]})`
+            // context.fillRect(canvas.width - 200, 0, 150, 150);
+        }
+        const startEvent = new Date();
+        const image = context.getImageData(0, 0, width, height);
+        const { data } = image;
+        const { length } = data;
+            
+        let percent = 0;
+
+        for (let i = 0; i < length; i += 4) { // red, green, blue, and alpha
+            pixel_target = rgb2lab([data[i], data[i+1], data[i+2]]);
+            const calculus = Math.round((i/length)*100);
+            if(percent !== calculus) {
+                percent = calculus;
+                console.log(`${percent}`);
+            }
+
+            const deltaEValue = deltaE(xBase, pixel_target);
+
+            if(deltaEValue > 45) {
+                const greyscale = 0.299*data[i] + 0.587*data[i+1] + 0.114*data[i+2]; 
+
+                data[i + 0] = greyscale;
+                data[i + 1] = greyscale;
+                data[i + 2] = greyscale;                    
+            }
+        }
+            
+        context.putImageData(image, 0, 0);
+        const endEvent = new Date();
+        const secondsDelta = (endEvent.getTime() - startEvent.getTime()) / 1000;
+
+        console.log(`Opération résolue en ${secondsDelta} secondes !`);
     });
 
     
     function rgb2lab(rgb){
-        var r = rgb[0] / 255,
-            g = rgb[1] / 255,
-            b = rgb[2] / 255,
-            x, y, z;
+        let r = rgb[0] / 255;
+        let g = rgb[1] / 255;
+        let b = rgb[2] / 255;
+        let x, y, z;
     
         r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
         g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
@@ -70,6 +115,7 @@ window.onload = function() {
         }
     }
 
+    // CIE76 way
     function deltaE(x1, x2) {
         return Math.sqrt(
             Math.pow(x2.L - x1.L, 2) +
